f2e2ddc9c21e721a983ec95a79ea29fc36a9c1fc
anupriya2004 <143175529+anupriya2004@users.noreply.github.com>
2024-06-19 15:06:29 -0300

    Add files via upload

---
+++ ecom project/products.js
@@ -0,0 +1,21 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+function add_to_cart(){
+  let l=[];
+  let s=document.getElementsByTagName("img").src;
+  document.getElementsByClassName("cart-products").appendChild=s;
+
+}
